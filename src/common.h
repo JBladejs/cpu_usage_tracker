c5c3230faf953230fd148b5a0bd3663a043e1bae
Alan Ćwiek <JBladejs@gmail.com>
2022-08-18 19:12:17 +0200

    Type aliases added

---
+++ src/common.h
@@ -0,0 +1,22 @@
+//
+// Created by Alan Ćwiek on 8/18/22.
+//
+
+#ifndef CPU_USAGE_TRACKER_COMMON_H
+#define CPU_USAGE_TRACKER_COMMON_H
+
+#include "inttypes.h"
+
+typedef int8_t s8;
+typedef uint8_t u8;
+
+typedef int16_t s16;
+typedef int16_t u16;
+
+typedef int32_t s32;
+typedef int32_t u32;
+
+typedef int64_t s64;
+typedef int64_t u64;
+
+#endif //CPU_USAGE_TRACKER_COMMON_H
