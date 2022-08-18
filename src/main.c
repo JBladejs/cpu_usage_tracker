e403ba2648b81495908c9f9ff2b436c774fcca8a
Alan Ćwiek <JBladejs@gmail.com>
2022-08-18 19:12:37 +0200

    Simple reading from /proc/stat

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "common.h"
 
 int main() {
-    printf("Hello, World!\n");
+
+    FILE *fp;
+    s8 cpu_name[255];
+    fp = fopen("/proc/stat", "r");
+
+    if (fp == NULL) {
+        //TODO: handle file read error
+    } else {
+        s64 user, nice, system, idle, dummy;
+        fscanf(fp, "%s %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld",
+               cpu_name, &user, &nice, &system, &idle, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy);
+        printf("cpu: %s, user mode: %ld, nice mode: %ld, system mode: %ld, idle: %ld\n",
+               cpu_name, user, nice, system, idle);
+        fscanf(fp, "%s %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld",
+               cpu_name, &user, &nice, &system, &idle, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy);
+        printf("cpu: %s, user mode: %ld, nice mode: %ld, system mode: %ld, idle: %ld\n",
+               cpu_name, user, nice, system, idle);
+    }
     return 0;
 }
