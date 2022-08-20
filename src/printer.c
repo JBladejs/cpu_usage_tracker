4451090046ea94247df0aa1d6b0b219bc5cdd95f
Alan Ćwiek <JBladejs@gmail.com>
2022-08-20 15:33:11 +0200

    Using queue for printer

--- src/printer.c
+++ src/printer.c
@@ -7,20 +7,23 @@
 #include <unistd.h>
 #include <bits/types/sig_atomic_t.h>
 #include "printer.h"
+#include "queue.h"
 
 volatile static sig_atomic_t running = FALSE;
-static f32 *usage = NULL;
+static struct Queue *queue = NULL;
 static u16 core_count = 1;
 
 void *printer_init(void *arg) {
     running = TRUE;
+    queue = queue_create(255, core_count * sizeof (f32));
     while (running) {
         system("clear");
-        if (usage != NULL) {
+        if (!queue_is_empty(queue)) {
+            f32 *usage = queue_dequeue(queue);
             for (int i = 0; i < core_count; ++i) {
                 printf("Core %d: %.2f%%\n", i, usage[i]);
             }
-            //TODO: free memory
+            free(usage);
         }
         sleep(1);
     }
@@ -28,7 +31,7 @@ void *printer_init(void *arg) {
 }
 
 void printer_add_data(f32 *values) {
-    usage = values;
+    queue_enqueue(queue, values);
 }
 
 void printer_set_core_count(u16 value) {
@@ -37,6 +40,8 @@ void printer_set_core_count(u16 value) {
 
 void printer_destroy() {
     running = FALSE;
+    free(queue);
+    queue = NULL;
 }
 
 
