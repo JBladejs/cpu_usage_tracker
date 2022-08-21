7464afd8c3ff6f40b5f3364da390669ad462b627
Alan Ćwiek <JBladejs@gmail.com>
2022-08-21 23:12:36 +0200

    Added dates to logfile

--- src/file_io/logfile.c
+++ src/file_io/logfile.c
@@ -3,6 +3,7 @@
 //
 
 #include <malloc.h>
+#include <time.h>
 #include "logfile.h"
 
 struct Logfile *logfile_init(char *file_name) {
@@ -10,11 +11,16 @@ struct Logfile *logfile_init(char *file_name) {
     logfile->file_name = file_name;
     logfile->file = fopen(file_name, "a+");
     if (logfile->file == NULL) return NULL;
+    logfile->time = time(NULL);
     return logfile;
 }
 
 void logfile_write(struct Logfile *logfile, char *message) {
-    fprintf(logfile->file, "%s\n", message);
+    struct tm *time_info = localtime(&logfile->time);
+    char time_string[255];
+    strftime(time_string, 255, "%T %D", time_info);
+    fprintf(logfile->file, "[%s] %s\n",
+            time_string, message);
 }
 
 void logfile_destroy(struct Logfile *logfile) {
--- src/file_io/logfile.h
+++ src/file_io/logfile.h
@@ -6,11 +6,13 @@
 #define CPU_USAGE_TRACKER_LOGFILE_H
 
 #include <bits/types/FILE.h>
+#include <bits/types/time_t.h>
 #include "../common.h"
 
 struct Logfile {
     char *file_name;
     FILE *file;
+    time_t time;
 };
 
 struct Logfile *logfile_init(char *file_name);
