55983b859754b91ef210e5a305a1d0d8c07c3b1d
Alan Ćwiek <JBladejs@gmail.com>
2022-08-23 00:24:17 +0200

    Added thread managment and thread id initialization

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -8,15 +8,50 @@
 #include "thread.h"
 #include "buffer.h"
 
+struct ThreadManager {
+    struct Thread *threads[10];
+    u8 index;
+
+    //padding
+    u64 : 56;
+};
+
+static struct ThreadManager *thread_manager_instance(void) {
+    static struct ThreadManager manager;
+    static u8 is_initialized = FALSE;
+
+    if (!is_initialized) {
+        manager.index = 0;
+    }
+
+    return &manager;
+}
+
+static pthread_t thread_manager_get_next_id(void) {
+    return thread_manager_instance()->index++;
+}
+
+static void thread_manager_add_thread(struct Thread *thread) {
+    if (thread->thread_id > 9) return;
+    thread_manager_instance()->threads[thread->thread_id] = thread;
+}
+
+static void thread_manager_destroy_all(void) {
+    struct ThreadManager *manager = thread_manager_instance();
+    for (s8 i = 10; i >= 0; --i) {
+        thread_destroy(manager->threads[i]);
+    }
+}
+
 struct Thread *thread_create(char *name, void *(*start)(struct Thread *), struct Buffer *read_buffer, struct Buffer *write_buffer) {
-    pthread_t thread_id;
     struct Thread *thread = malloc(sizeof(struct Thread));
+    thread->thread_id = thread_manager_get_next_id();
     thread->name = name;
     thread->timer = 0;
     thread->start_routine = start;
-    thread->thread_id = thread_id;
     thread->read_buffer = read_buffer;
     thread->write_buffer = write_buffer;
+    thread_manager_add_thread(thread);
     return thread;
 }
 
@@ -66,4 +101,13 @@ void thread_write_to_buffer(struct Thread *thread, void *data) {
 void *thread_read_from_buffer(struct Thread *thread) {
     if (thread->read_buffer != NULL)return buffer_pop(thread->read_buffer);
     else return NULL;
+}
+
+void thread_destroy(struct Thread *thread) {
+    thread_stop(thread);
+    free(thread);
+}
+
+void thread_destroy_all(void) {
+    thread_manager_destroy_all();
 }
