e3808a7fe7c5866483ece5d8651e56c9f8f37f20
Alan Ćwiek <JBladejs@gmail.com>
2022-08-22 19:22:15 +0200

    Restored all threads

--- src/main.c
+++ src/main.c
@@ -5,8 +5,6 @@
 #include <signal.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
 #include "threads/analyzer.h"
 #include "threads/printer.h"
 #include "file_io/statfile.h"
@@ -30,25 +28,25 @@ int main() {
     }
     u16 core_count = statfile_get_core_count(statfile);
 
-//    logger_init();
+    logger_init();
     printer_init(core_count);
     struct Buffer *read_data = BUFFER_ARRAY_NEW(struct CpuStats, core_count, 5);
     reader_init(statfile, read_data);
     analyzer_init(core_count, read_data);
 
-//    struct Thread **threads = malloc(sizeof (struct Thread) * 4);
-//    threads[0] = reader_get_thread();
-//    threads[1] = analyzer_get_thread();
-//    threads[2] = printer_get_thread();
-//    threads[3] = logger_get_thread();
-//
-//    watchdog_init(threads, 4);
+    struct Thread **threads = malloc(sizeof (struct Thread) * 4);
+    threads[0] = reader_get_thread();
+    threads[1] = analyzer_get_thread();
+    threads[2] = printer_get_thread();
+    threads[3] = logger_get_thread();
 
-//    thread_join(watchdog_get_thread());
+    watchdog_init(threads, 4);
+
+    thread_join(watchdog_get_thread());
     thread_join(reader_get_thread());
     thread_join(analyzer_get_thread());
     thread_join(printer_get_thread());
-//    thread_join(logger_get_thread());
+    thread_join(logger_get_thread());
 
     buffer_destroy(read_data);
 
